49e63668e03d34f63e48938a121d6e9590015c01
Grave <kyuvcpavm@mail.ru>
2022-02-21 21:51:46 +0300

    review changes

--- routes/users.js
+++ routes/users.js
@@ -7,9 +7,9 @@ const {
   updateUserAvatar
 } = require('../controllers/users');
 
-router.get('/users', getUsers);
-router.get('/users', getUserById);
-router.post('/users', createUser);
+router.get('/', getUsers);
+router.get('/:id', getUserById);
+router.post('/', createUser);
 router.patch('/me', updateUserInfo);
 router.patch('/me/avatar', updateUserAvatar);
 
